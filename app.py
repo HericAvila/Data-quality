09ca748e8ddc48c735b125b6fcd7f3f196d62b23
Héric Ávila <avilaheric@ucl.br>
2023-10-30 08:52:26 -0300

    Atualizando streamlit

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # app.py
-
 import streamlit as st
 import pandas as pd
 from pydantic import ValidationError
-from models import Vendas
+from models import Vendas  # Ajuste o caminho se necessário
 
 def validar_dados(dataframe):
     """
-    Validadar dados do modelo Pydantic
+    Valida os dados com base no modelo Pydantic.
     """
     erros = []
     for _, row in dataframe.iterrows():  # Iterando através das linhas do DataFrame
@@ -15,30 +14,34 @@ def validar_dados(dataframe):
             # Convertendo a linha do DataFrame para um dicionário e validando
             Vendas(**row.to_dict())
         except ValidationError as e:
-            erros.append(e.json())
+            erros.append(e.errors())
 
     if erros:
-        return False, erros
-    
+        return False, erros  # Retornando os erros se existirem
+
     return True, "Todos os registros são válidos!"  # Retornando mensagem de sucesso
 
-    uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx'])
+# Título da aplicação
+st.title('Validador de Dados de Vendas')
 
-    if uploded_file:
-        try:
-            # Ler o arquivo Excel com o Pandas
-            df = pd.read_excel(uploaded_file, engine='openpyxl',index_col=none)
+# Upload do arquivo Excel
+uploaded_file = st.file_uploader("Escolha um arquivo Excel", type=['xlsx'])
 
-            # Validação dos dados com o esquema Pydantic
-            sucesso, mensagem = validar_dados(df) # passando o DataFrame para a função validar_dados
+if uploaded_file:
+    try:
+        # Ler o arquivo Excel com o Pandas
+        df = pd.read_excel(uploaded_file, engine='openpyxl', index_col=None)
 
-            if sucesso:
-                st.success(mensagem)
-            else:
-                st.error('Erro de validaçao	encontrado!')
-                for erro in mensagem: # Iterando sobre cada erro e imprimindo na tela
-                    st.json(erro) # Imprimindo o erro em formato JSON
+        # Validação dos dados com o esquema Pydantic
+        sucesso, mensagem = validar_dados(df)  # Passando o DataFrame diretamente
 
+        # Exibindo os resultados da validação
+        if sucesso:
+            st.success(mensagem)  # Aqui usamos a mensagem de retorno da validação
+        else:
+            st.error("Erro de validação encontrado:")
+            for erro in mensagem:  # Iterando sobre cada erro e imprimindo-o
+                st.json(erro)  # Mostrando os detalhes do erro
 
-        except:
-            st.error(f"Ocorreu um erro:{e}")
+    except Exception as e:
+        st.error(f"Ocorreu um erro: {e}")
